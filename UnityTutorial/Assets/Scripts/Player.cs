788cfb442a1760049bf6f6608c336279889bb185
Jun-Young Noh <119721103+junyoungnnn@users.noreply.github.com>
2023-12-11 01:41:41 +0900

    (2023.12.08) 외부에서 애니메이션 가져오기

---
+++ UnityTutorial/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+
+        rigidbody.useGravity = false;
+    }
+
+    void Update()
+    {
+        
+    }
+}
