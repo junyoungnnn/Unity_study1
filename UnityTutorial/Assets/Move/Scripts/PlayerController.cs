6a59def50b5d67d5c89a4533e0e9efa5eb749ae7
Jun-Young Noh <119721103+junyoungnnn@users.noreply.github.com>
2023-12-12 19:47:33 +0900

    (2023.12.12) 게임 오브젝트 이동 시스템

---
+++ UnityTutorial/Assets/Move/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector3 direction;
+    public float speed = 5f;
+   
+
+    void Update()
+    {
+        // Input.GetAxis() : 특정한 key를 누를 때 -1 ~ 1 사이의 값을 반환하는 함수입니다.
+        direction.x = Input.GetAxis("Horizontal");
+        direction.z = Input.GetAxis("Vertical");
+
+        // 백터의 정규화
+        direction.Normalize();
+
+        // Time.deltaTime : 이전 프레임이 완료되는 데까지 걸린 시간을 의미합니다.
+        transform.position += direction * speed * Time.deltaTime;
+    }
+}
