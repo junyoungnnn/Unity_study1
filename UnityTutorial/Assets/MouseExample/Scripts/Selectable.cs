8e6fb90f08ca57aac446658608aa3a1d68d453e5
Jun-Young Noh <119721103+junyoungnnn@users.noreply.github.com>
2024-02-05 21:07:46 +0900

    (2024.02.05) 게임 오브젝트 병합 시스템 제작

---
+++ UnityTutorial/Assets/MouseExample/Scripts/Selectable.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Selectable : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    [SerializeField] ParticleSystem particle;
+
+    private bool select;
+
+    void Start()
+    {
+        select = true;
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void OnMouseDown()
+    {
+        particle.Play();
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 mousePosition = new Vector3
+        (
+            Input.mousePosition.x,
+            Input.mousePosition.y,
+            Camera.main.WorldToScreenPoint(gameObject.transform.position).z
+        );
+
+        transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
+    }
+
+    private void OnMouseExit()
+    {
+        select = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        string selectObject = transform.name;
+        string collisionObject = other.transform.name;
+
+        if (selectObject == collisionObject)
+        {
+            int add = int.Parse(collisionObject.Substring(collisionObject.Length - 1)) + int.Parse(selectObject.Substring(selectObject.Length - 1));
+
+            if (select)
+            {
+                Instantiate(Resources.Load<GameObject>("Container " + add));
+            }
+            Destroy(gameObject);
+            Destroy(other.gameObject);
+        }
+
+    }
+}
